168efb55bc4929248ef9b888b810eefd27b42c1c
rernesto <rernesto.espinosa@gmail.com>
2024-02-25 21:01:58 -0300

    feat (app): initial files
    Security microservice files
    
    Refs: #1

---
+++ config/bundles.php
@@ -0,0 +1,6 @@
+<?php
+
+return [
+    EasyCorp\Bundle\EasyAdminBundle\EasyAdminBundle::class => ['all' => true],
+    Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
+];
---
+++ src/Controller/DefaultController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Security\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Routing\Attribute\Route;
+
+class DefaultController extends AbstractController
+{
+    #[Route('/', name: 'security_default_index')]
+    public function index(): JsonResponse
+    {
+        return $this->json([
+            'message' => 'Welcome to your new security controller!',
+            'path' => 'apps/security/src/Controller/DefaultController.php',
+        ]);
+    }
+}
